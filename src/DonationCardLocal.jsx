5f13147c086260c993a3f42462c9f3a057823218
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-09-25 15:47:45 +0600

    added style in donation card

--- src/DonationCardLocal.jsx
+++ src/DonationCardLocal.jsx
@@ -1,16 +1,16 @@
-
+import  PropTypes  from "prop-types";
 
 const DonationCardLocal = ({donation}) => {
-    const {Picture, Category, Card_bg_Color, Title, Text_Button_bg_Color, Price, Description
+    const {Picture, Category_bg_Color, Category, Card_bg_Color, Title, Text_Button_bg_Color, Price
     } = donation;
     return (
         <div className="rounded-lg" style={{backgroundColor: Card_bg_Color}}>
             <div className="flex gap-x-7">
             <img src={Picture} alt="" />
             <div className="justify-center flex-col flex">
-              <h1>{Category}</h1>
-              <h1>{Title}</h1>
-              <p>{Price}</p>
+              <h1 className="w-fit px-3 rounded-md" style={{backgroundColor: Category_bg_Color,color: Text_Button_bg_Color}}>{Category}</h1>
+              <h1 className="text-xl font-semibold my-1">{Title}</h1>
+              <p className="text-xl" style={{color: Text_Button_bg_Color}}>${Price}</p>
             </div>
             </div>
 
@@ -18,4 +18,7 @@ const DonationCardLocal = ({donation}) => {
     );
 };
 
+DonationCardLocal.propTypes ={
+  donation: PropTypes.object.isRequired
+}
 export default DonationCardLocal;
