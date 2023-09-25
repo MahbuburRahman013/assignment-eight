2ca8249c9c1f26c54f279043729e64bbf9ccc680
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-09-25 22:47:50 +0600

    added responsive design for 404 page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,10 +3,10 @@
 const ErrorPage = () => {
     return (
         <div className="h-screen flex flex-col gap-y-7 justify-center items-center">
-            <div className="text-7xl font-bold text-red-400">
+            <div className="md:text-7xl text-2xl font-bold text-red-400">
                   404
             </div>
-            <h1 className="text-7xl font-bold text-gray-400">Page Is Not Found.</h1>
+            <h1 className="md:text-7xl text-2xl font-bold text-gray-400">Page Is Not Found.</h1>
         </div>
     );
 };
