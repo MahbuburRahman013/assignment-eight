5813969861e2bf88cae4e4382fd6dcba03739373
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-09-26 16:12:55 +0600

    change the pie chart

--- src/PieChart.jsx
+++ src/PieChart.jsx
@@ -1,61 +1,38 @@
-import { Chart } from "chart.js/auto";
-import { useEffect, useRef, useState } from "react";
+
+import { useEffect, useState } from "react";
+import Chart from 'react-apexcharts';
+
+
 const PieChart = () => {
-     
-     const chartRef = useRef(null)
-     const chartInstance = useRef(null)
-     const [localData,setLocalData] = useState()
-     const [allData,setAllData] = useState()
+
+
+    const [localData,setLocalData] = useState()
+    const [allData,setAllData] = useState()
+
+    useEffect(()=>{
+        const dataStr = localStorage.getItem('data')
+        const dataL = JSON.parse(dataStr);
+       dataL ? setLocalData(dataL.length) : setLocalData(0);
+      dataL ? setAllData(12 - dataL.length) : setAllData(12);
+    },[])
     
 
-useEffect(()=>{
-    fetch('donation.json')
-    .then(res=> res.json())
-    .then(data=> data ? setAllData(data.length) : setAllData(12))
-},[])
-
-useEffect(()=>{
-    const dataStr = localStorage.getItem('data')
-    const dataL = JSON.parse(dataStr);
-   dataL ? setLocalData(dataL.length) : setAllData(0)
-},[])
-
-console.log(localData)
-
-     useEffect(()=>{
-               if(chartInstance.current){
-                chartInstance.current.destroy()
-               }
-               const myChartRef = chartRef.current.getContext('2d');
-
-               chartInstance.current = new Chart(myChartRef,{
-                type : 'pie',
-                data : {
-                    labels: ['Your Donation', 'Total Donation'],
-                    datasets: [
-                        {
-                            data: [localData, allData],
-                            backgroundColor: [
-                              '#ff4848',
-                              '#50e750',
-                            ],
-                        }
-                    ]
-                    
-                }
-            })
-               return ()=>{
-                if(chartInstance.current){
-                    chartInstance.current.destroy()
-                }
-               }
-     },[localData, allData])
-
     return (
-        <div className="flex justify-center items-center">
-            <div className="md:w-[500px] lg:w-[500px] w-[300px]">
-           <canvas ref={chartRef} />
-        </div>
+        <div className="flex justify-center container mx-auto items-center">
+            <div className="w-full h-full">
+            <Chart  type='pie'
+            width='100%'
+            height='100%'
+            series={[localData,allData]}
+            
+            options={{
+                labels:['Your Donation','Total Donation']
+            }}
+
+            >
+
+            </Chart>
+            </div>
         </div>
     );
 };
